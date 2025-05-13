8e44d579b5d9b762174251a0fdd3d0d4c73a3b17
Will Bradshaw <wjbradshaw1@gmail.com>
2025-05-13 15:20:36 +0000

    Added missing functionality to LCA_TSV Python script

--- modules/local/lcaTsv/resources/usr/bin/lca_tsv.py
+++ modules/local/lcaTsv/resources/usr/bin/lca_tsv.py
@@ -16,6 +16,7 @@ import argparse
 from datetime import datetime, timezone
 import gzip
 import bz2
+from dataclasses import dataclass
 
 # Configure logging
 class UTCFormatter(logging.Formatter):
@@ -32,6 +33,17 @@ logger.addHandler(handler)
 
 # Define constants
 TAXID_ROOT = 1
+
+# Define Group class
+@dataclass
+class Group:
+    group_id: str
+    taxids: set[int]
+    n_entries: int
+    min_score: float
+    max_score: float
+    mean_score: float
+    lca: int | None
 
 #=======================================================================
 # Auxiliary functions
@@ -214,6 +226,207 @@ def find_lca_set(
     # Return the LCA and the updated path cache
     return lca, path_cache
 
+def new_group(
+        group_id: str,
+        taxid: int,
+        score: float,
+        ) -> Group:
+    """
+    Create a new Group object from minimal input information.
+    Args:
+        group_id (str): Group ID.
+        taxid (int): Taxid.
+        score (float): Score.
+    Returns:
+        Group: New Group object.
+    """
+    return Group(
+        group_id=group_id,
+        taxids={taxid},
+        n_entries=1,
+        min_score=score,
+        max_score=score,
+        mean_score=score,
+        lca=None,
+    )
+
+def process_input_line(
+        fields: list[str],
+        group_idx: int,
+        taxid_idx: int,
+        score_idx: int,
+        group_info: Group | None,
+        ) -> Group:
+    """
+    Process a single line of an input TSV and return an updated Group object
+    containing information about the corresponding entry group.
+    Args:
+        fields (list[str]): List of fields from an input line.
+        group_idx (int): Index of the group field in the input line.
+        taxid_idx (int): Index of the taxid field in the input line.
+        score_idx (int): Index of the score field in the input line.
+        group_info (Group | None): Group object containing information about the
+            corresponding entry group.
+    Returns:
+        Group: Updated Group object containing information about the
+            corresponding entry group.
+    """
+    # Get group ID and confirm match with Group object
+    group_id = fields[group_idx]
+    if group_info is not None:
+        assert group_id == group_info.group_id, \
+            f"Group ID mismatch: {group_id} != {group_info.group_id}"
+    # Get taxid and score
+    taxid = int(fields[taxid_idx])
+    score = float(fields[score_idx])
+    # If no Group object, create one
+    if group_info is None:
+        return new_group(group_id, taxid, score)
+    # Otherwise, update Group object
+    min_score = min(group_info.min_score, score)
+    max_score = max(group_info.max_score, score)
+    score_sum = group_info.mean_score * group_info.n_entries + score
+    mean_score = score_sum / (group_info.n_entries + 1)
+    return Group(
+        group_id=group_id,
+        taxids=group_info.taxids.add(taxid),
+        n_entries=group_info.n_entries + 1,
+        min_score=min_score,
+        max_score=max_score,
+        mean_score=mean_score,
+        lca=group_info.lca,
+    )
+
+def process_group_to_output(
+        group_info: Group,
+        child_to_parent: dict[int, int],
+        path_cache: dict[int, list[int]],
+    ) -> tuple[str, dict[int, list[int]]]:
+    """
+    Process a Group object and return a string for output.
+    Args:
+        group_info (Group): Group object to process.
+        child_to_parent (dict[int, int]): Dictionary mapping each taxid to its parent.
+        path_cache (dict[int, list[int]]): Cache of precomputed paths to the root.
+    Returns:
+        tuple[str, dict[int, list[int]]]: Tuple containing the output line
+            and the updated path cache.
+    """
+    # Get LCA of taxids in group
+    lca, path_cache = find_lca_set(group_info.taxids, child_to_parent, path_cache)
+    group_info.lca = lca
+    # Prepare output line
+    out_fields = [
+        group_info.group_id,
+        str(lca),
+        str(group_info.n_entries),
+        str(group_info.min_score),
+        str(group_info.max_score),
+        str(group_info.mean_score),
+    ]
+    out_line = "\t".join(out_fields) + "\n"
+    # Return output line and updated path cache
+    return out_line, path_cache
+
+def get_output_header() -> str:
+    """
+    Get the header for the output TSV.
+    Returns:
+        str: Header for the output TSV.
+    """
+    fields = ["group_id", "lca", "n_entries", "min_score", "max_score", "mean_score"]
+    return "\t".join(fields) + "\n"
+
+def parse_input_header(
+        header_fields: list[str],
+        group_field: str,
+        taxid_field: str,
+        score_field: str,
+        ) -> tuple[int, int, int]:
+    """
+    Parse the header of an input TSV and return the indices of the group,
+    taxid, and score fields.
+    Args:
+        header_fields (list[str]): List of fields from the header of an input TSV.
+        group_field (str): Column header for group field.
+        taxid_field (str): Column header for taxid field.
+        score_field (str): Column header for score field.
+    Returns:
+        tuple[int, int, int]: Tuple containing the indices of the group,
+            taxid, and score fields.
+    """
+    # Confirm fields are present
+    assert group_field in header_fields, f"Group field {group_field} not found in header."
+    assert taxid_field in header_fields, f"Taxid field {taxid_field} not found in header."
+    assert score_field in header_fields, f"Score field {score_field} not found in header."
+    # Return indices
+    group_idx = header_fields.index(group_field)
+    taxid_idx = header_fields.index(taxid_field)
+    score_idx = header_fields.index(score_field)
+    return group_idx, taxid_idx, score_idx
+
+def parse_input_tsv(
+        input_path: str,
+        output_path: str,
+        group_field: str,
+        taxid_field: str,
+        score_field: str,
+        child_to_parent: dict[int, int],
+        ) -> None:
+    """
+    Iterate linewise over input TSV, calculating the LCA for each group
+    of entries and writing the result to the output file.
+    Args:
+        input_path (str): Path to input TSV.
+        output_path (str): Path to output TSV.
+        group_field (str): Column header for group field.
+        taxid_field (str): Column header for taxid field.
+        score_field (str): Column header for score field.
+        child_to_parent (dict[int, int]): Dictionary mapping each taxid to its parent.
+    """
+    with open_by_suffix(input_path) as inf, open_by_suffix(output_path, "w") as outf:
+        # Write header to output file
+        outf.write(get_output_header())
+        # Read header from input file
+        header = inf.readline().strip().split("\t")
+        # Get indices of fields
+        group_idx, taxid_idx, score_idx = parse_input_header(
+            header, group_field, taxid_field, score_field)
+        def parse_line(fields: list[str], group_info: Group | None) -> Group:
+            return process_input_line(fields, group_idx, taxid_idx, score_idx, group_info)
+        # Process first line
+        fields = inf.readline().strip().split("\t")
+        group_id = fields[group_idx]
+        group_info = parse_line(fields, None)
+        # Iterate over input file
+        n_entries = 1
+        n_groups = 1
+        path_cache = {}
+        for line in inf:
+            # Parse fields
+            fields = line.strip().split("\t")
+            # Get group ID and check sorting
+            group_id_new = fields[group_idx]
+            assert group_id_new >= group_id, \
+                f"Group ID out of order: {group_id_new} < {group_id}"
+            # If group ID is unchanged, process line into existing Group object
+            if group_id_new == group_id:
+                group_info = parse_line(fields, group_info)
+            # Otherwise, write previous Group object to output and start new one
+            else:
+                out_line, path_cache = process_group_to_output(
+                    group_info, child_to_parent, path_cache)
+                outf.write(out_line)
+                group_info = parse_line(fields, None)
+                group_id = group_id_new
+                n_groups += 1
+            n_entries += 1
+        # Write last Group object to output
+        out_line, path_cache = process_group_to_output(
+            group_info, child_to_parent, path_cache)
+        outf.write(out_line)
+        logger.info(f"Processed {n_entries} entries in {n_groups} groups.")
+
 #=======================================================================
 # Main function
 #=======================================================================
@@ -221,10 +434,16 @@ def find_lca_set(
 def main() -> None:
     logger.info("Initializing script.")
     # Parse arguments
+    logger.info("Parsing arguments.")
     args = parse_args()
     # Import taxonomy DB and process into dictionaries
+    logger.info("Parsing taxonomy DB.")
     child_to_parent, parent_to_children = parse_taxonomy_db(args.db)
-
+    logger.info(f"Parsed taxonomy information for {len(child_to_parent)} taxids.")
+    # Parse input TSV and write LCA information to output TSV
+    logger.info("Parsing input TSV.")
+    parse_input_tsv(args.input, args.output, args.group, args.taxid, args.score, child_to_parent)
+    # Log completion
     logger.info("Script complete.")
 
 if __name__ == "__main__":
