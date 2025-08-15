a1d509a21641a518d63ee89841d9a6a1be2b8861
harm0n <harmonprograms@protonmail.com>
2025-08-15 13:42:43 -0400

    removing unhelpful comments

--- modules/local/validateGrouping/resources/usr/bin/test_validate_grouping.py
+++ modules/local/validateGrouping/resources/usr/bin/test_validate_grouping.py
@@ -46,13 +46,9 @@ def test_validate_grouping_empty_files():
         write_tsv(grouping_path, ['group', 'sample'], [['g1', 'S1'], ['g2', 'S2']])
         write_tsv(virus_hits_path, ['sample'], [])  # Empty data, just header
         validate_grouping(grouping_path, virus_hits_path, samples_without_vv_hits, validated_output)
-
-        # Validated output should only have header, no data
         with open(validated_output) as f:
             validated_lines = [l.strip() for l in f if l.strip()]
         assert validated_lines == ['group\tsample']
-
-        # All grouping samples should be in samples_without_vv_hits
         with open(samples_without_vv_hits) as f:
             unused_lines = [l.strip() for l in f if l.strip()]
         assert set(unused_lines) == {'S1', 'S2'}
@@ -70,16 +66,10 @@ def test_validate_grouping_perfect_match():
         write_tsv(grouping_path, ['group', 'sample'], [['g1', 'S1'], ['g1', 'S2'], ['g2', 'S3']])
         write_tsv(virus_hits_path, ['sample'], [['S1'], ['S2'], ['S3']])
         validate_grouping(grouping_path, virus_hits_path, samples_without_vv_hits, validated_output)
-
-        # Read validated output
         with open(validated_output) as f:
             validated_lines = [l.strip() for l in f if l.strip()]
-        
-        # Should have header plus all 3 samples (sorted by sample name)
         expected_lines = ['group\tsample', 'g1\tS1', 'g1\tS2', 'g2\tS3']
         assert validated_lines == expected_lines
-
-        # No samples should be without viral hits
         with open(samples_without_vv_hits) as f:
             unused_lines = [l.strip() for l in f if l.strip()]
         assert unused_lines == []
